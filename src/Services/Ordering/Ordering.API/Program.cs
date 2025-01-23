f333c94a4a4346d05e0da501671b336111fc1925
Mustafa Alkan <mustafa.alkan@eczacibasi.com.tr>
2025-01-24 00:14:21 +0300

    Ordering API Project created

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,23 +1,24 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseApiServices();
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    await app.InitialiseDatabaseAsync();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
